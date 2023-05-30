b0a963c2bf951c47b0c1fed436620d138c8f68e1
Oluwakayode Michael <michaelaykay@gmail.com>
2023-05-29 19:11:18 -0700

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,17 +2,25 @@
 """
 Starts the API server
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os 
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON response"""
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
